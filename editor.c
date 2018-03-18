12c1d95bec5fa302568663dc20c9a941ab4d20e3
bowei <bowei.han100@gmail.com>
2018-03-18 16:30:02 -0400

    implement incremental search

--- editor.c
+++ editor.c
@@ -73,7 +73,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** Terminal ***/
 
@@ -381,7 +381,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
 	if (E.filename == NULL) {
-		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
 		if (E.filename == NULL) {
 			editorSetStatusMessage("Save aborted");
 			return;
@@ -411,9 +411,10 @@ void editorSave() {
 
 /*** find ***/
 
-void editorFind() {
-	char *query = editorPrompt("Search: %s (ESC to cancel)");
-	if (query == NULL) return;
+void editorFindCallback(char *query, int key) {
+	if (key == '\r' || key == '\x1b') {
+		return;
+	}
 
 	int i;
 	for (i = 0; i < E.numrows; i++) {
@@ -426,8 +427,13 @@ void editorFind() {
 			break;
 		}
 	}
+}
 
-	free(query);
+void editorFind() {
+	char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+	if (query) {
+		free(query);
+	}
 }
 
 /*** Append Buffer ***/
@@ -458,7 +464,7 @@ void abFree(struct abuf *ab) {
 
 /*** Input ***/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
 	size_t bufsize = 128;
 	char *buf = malloc(bufsize);
 
@@ -475,11 +481,13 @@ char *editorPrompt(char *prompt) {
 		} else if (c == '\x1b') {
 			// cancel saveAs with escape
 			editorSetStatusMessage("");
+			if (callback) callback(buf, c);
 			free(buf);
 			return NULL;
 		} else if (c == '\r') {
 			if (buflen != 0) {
 				editorSetStatusMessage("");
+				if (callback) callback(buf, c);
 				return buf;
 			}
 		} else if (!iscntrl(c) && c < 128) { // don't process special keys
@@ -490,6 +498,8 @@ char *editorPrompt(char *prompt) {
 			buf[buflen++] = c;
 			buf[buflen] = '\0';
 		}
+
+		if (callback) callback(buf, c);
 	}
 }
 
