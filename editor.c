d209d0ba1930479e3d24a68766bff119040a10c0
bowei <bowei.han100@gmail.com>
2018-03-18 00:36:21 -0400

    make sure tabs works in all cases

--- editor.c
+++ editor.c
@@ -18,6 +18,8 @@
 /*** Constant/Macro Definitions ***/
 
 #define EDITOR_VERSION "0.0.1"
+#define EDITOR_TAB_STOP 8
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
@@ -37,7 +39,9 @@ enum editorKey {
 
 typedef struct erow {
 	int size;
+	int rsize;
 	char *chars;
+	char *render;
 } erow;
 
 struct editorConfig {
@@ -169,6 +173,29 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+void editorUpdateRow(erow *row) {
+	int tabs = 0;
+	int j;
+	for (j = 0; j < row->size; j++) {
+		if (row->chars[j] == '\t') tabs++;
+	}
+
+	free(row->render);
+	row->render = malloc(row->size + tabs*(EDITOR_TAB_STOP - 1) + 1);
+
+	int idx = 0;
+	for (j = 0; j < row->size; j++) {
+		if (row->chars[j]=='\t') {
+			row->render[idx++] = ' ';
+			while (idx % EDITOR_TAB_STOP != 0) row->render[idx++] = ' ';
+		} else {
+			row->render[idx++] = row->chars[j];
+		}
+	}
+	row->render[idx] = '\0';
+	row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
 	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 	int at = E.numrows;
@@ -176,6 +203,11 @@ void editorAppendRow(char *s, size_t len) {
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
 	E.row[at].chars[len] = '\0';
+
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+	editorUpdateRow(&E.row[at]);
+
 	E.numrows++;
 }
 
@@ -336,10 +368,10 @@ void editorDrawRows(struct abuf *ab) {
 				abAppend(ab, "~", 1);
 			}
 		} else {
-			int len = E.row[filerow].size - E.coloff;
+			int len = E.row[filerow].rsize - E.coloff;
 			if (len < 0) len = 0;
 			if (len > E.screencols) len = E.screencols;
-			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+			abAppend(ab, &E.row[filerow].render[E.coloff], len);
 		}
 		// clear each line as we redraw instead of clearing entire screen
 		abAppend(ab, "\x1b[K", 3);
