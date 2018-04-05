69a89fe14b72438f7525a2454524abaf3a2747a8
bowei <bowei.han100@gmail.com>
2018-04-05 17:32:05 +0000

    add some comments

--- src/find.h
+++ src/find.h
@@ -1,8 +1,11 @@
 #ifndef __FIND_H__
 #define __FIND_H__
 
+// let user search forward and backwards through matched
+// search terms using arrow keys
 void editorFindCallback(char *query, int key);
 
+// prompt user for search term and enter search mode
 void editorFind();
 
 #endif
--- src/input.h
+++ src/input.h
@@ -1,10 +1,13 @@
 #ifndef __INPUT_H__
 #define __INPUT_H__
 
+// prompt user in status message bar
 char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
+// move cursor based on keypress and ensure cursor says within text
 void editorMoveCursor(int key);
 
+// process a user input keypress (raw mode)
 void editorProcessKeypress();
 
 #endif
